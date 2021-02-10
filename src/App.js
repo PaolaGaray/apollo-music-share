63d63a834f2abbf4fab431bfaaf10b2ce720f973
Paola Garay <garay.angela@gmail.com>
2021-02-10 08:54:34 -0500

    State management setup using useReducer and usecontext hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 import { Grid, useMediaQuery } from '@material-ui/core';
 
 import Header from './components/Header';
 import SongList from './components/SongList';
 import SongPlayer from './components/SongPlayer';
 import AddSong from './components/AddSong';
+import songReducer from './reducer';
+
+
+export const SongContext = createContext({
+  song: {
+    id: "252db9db-7e9d-44c4-be84-f6a394ce5a32",
+    title: "YokoO - DHA Mix #267",
+    artist: "DHA FM (Deep House Amsterdam)",
+    thumbnail: "https://i1.sndcdn.com/artworks-000209830520-2e3r67-t500x500.jpg",
+    url: "https://soundcloud.com/deep-house-amsterdam/yokoo-dha-mix-267",
+    duration: 11202.957
+  },
+  isPlaying: false
+});
 
 
 function App() {
+  const initialSongState = useContext(SongContext);
+  const [state, dispatch] = useReducer(songReducer, initialSongState);
+
   const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up('sm'));
   const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up('md'));
 
   return (
-    <>
+    <SongContext.Provider value={{ state, dispatch }}>
       {greaterThanSm && <Header />}
       <Grid container spacing={3}>
         <Grid
@@ -43,7 +60,7 @@ function App() {
           <SongPlayer />
         </Grid>
       </Grid>
-    </>
+    </SongContext.Provider>
   );
 }
 
---
+++ src/reducer.js
@@ -0,0 +1,8 @@
+const songReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default songReducer;
